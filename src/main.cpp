483402f268339985e7faeedd5507c1ae15425c01
Yichao Zhou <broken.zhoug@gmail.com>
2015-04-02 11:31:11 -0400

    1

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 #include "ui/ncurses.hpp"
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
 
     unique_ptr<UI> ui = make_unique<NcursesUI>();
     ui->run();
+
 
 	return 0;
 }
